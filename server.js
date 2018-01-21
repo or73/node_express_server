2be0b3bd5a9cbf578bd9230902fa9edff2a03168
OR <oreyesc@gmail.com>
2018-01-21 17:25:41 -0500

    Github && SSH Keys

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express   = require('express');
 const hbs       = require('hbs');
 const fs        = require('fs');
 
-
+const port      = process.env.EXPRESS_PORT || 3000;
 
 const app   = express();
 
@@ -79,9 +79,9 @@ app.get('/bad',
 			});
 		});
 
-app.listen(3000,
+app.listen(port,
 			() => {
-				console.log('Server is up on port 3000');
+				console.log(`Server is up on port ${ port }`);
 			});
 
 
